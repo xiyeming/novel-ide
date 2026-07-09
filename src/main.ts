def7adcc70720a585467613ccc93d8835e008cbb
XiYeMing <xiyeming@163.com>
2026-07-09 15:24:27 +0800

    style: add component styles according to UI Architecture spec

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import "./styles/main.css";
+import "./styles/components/index.css";
 
 const app = createApp(App);
 app.use(createPinia());
